402eb0c9ca608385d73884756ceb9acfba3c52ac
jejdacz <markhunter@seznam.cz>
2020-10-19 20:04:56 +0200

    Updates

---
+++ precache-manifest.ab1637b6ca4e34bd411510419f405423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14779a5ceeb987ed4f43b81bfdc38f4c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5cf98f06fbe0af48b5d0",
+    "url": "/monsters-rolodex/static/css/main.c9149050.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf98f06fbe0af48b5d0",
+    "url": "/monsters-rolodex/static/js/main.3f40c286.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41a3fafdc4a0dd5f588f56130af8cc7a.js"
+  "/monsters-rolodex/precache-manifest.ab1637b6ca4e34bd411510419f405423.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
